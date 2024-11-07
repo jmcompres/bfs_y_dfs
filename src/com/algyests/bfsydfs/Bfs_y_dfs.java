5d8568989e17f22a79fdec50c78b04a298a031d2
m_com <m_com@hotmail.com>
2024-11-07 17:44:00 -0400

    Implementación DFS (Todavía hay que arreglarlo)

--- src/com/algyests/bfsydfs/Bfs_y_dfs.java
+++ src/com/algyests/bfsydfs/Bfs_y_dfs.java
@@ -49,6 +49,28 @@ public class Bfs_y_dfs {
         return listaRecorrido;
     }
 
+    private static List<Character> recursionDFS(List<List<Character>> grafo, char nodoActual, boolean[] nodosVisitados)
+    {
+        nodosVisitados[nodoActual-'A'] = true;
+        List<Character> listaRecorrido = new LinkedList<Character>();
+
+        for (Character nodoAdyacente : grafo.get(nodoActual-'A'))
+            if (!nodosVisitados[nodoAdyacente-'A'])
+                listaRecorrido.addAll(recursionDFS(grafo, nodoAdyacente, nodosVisitados));
+
+        return listaRecorrido;
+    }
+
+    private static List<Character> dfs(List<List<Character>> grafo, char nodoInicial)
+    {
+        boolean[] nodosVisitados = new boolean[grafo.size()];
+        List<Character> listaRecorrido = new LinkedList<Character>();
+        listaRecorrido.add(nodoInicial);
+        listaRecorrido.addAll(recursionDFS(grafo, nodoInicial, nodosVisitados));
+
+        return listaRecorrido;
+    }
+
     public static void main(String[] args) {
 
         int[][] matriz = {{0,1,0,0,1,0,1,0,0,0},    //Matriz de int para no tener que escribir tantos trues y falses
@@ -67,6 +89,13 @@ public class Bfs_y_dfs {
         List<Character> recorridoBFS = bfs(listaAdyacencia, 'A');
         System.out.println("Recorrido BFS empezando desde el nodo A: ");
         for (Character nodoActual : recorridoBFS)
+            System.out.print(nodoActual+" ");
+
+        System.out.println("\n");
+
+        List<Character> recorridoDFS = dfs(listaAdyacencia, 'A');
+        System.out.println("Recorrido DFS empezando desde el nodo A: ");
+        for (Character nodoActual : recorridoDFS)
             System.out.print(nodoActual+" ");
 
     }
